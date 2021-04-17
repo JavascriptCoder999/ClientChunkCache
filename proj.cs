e1f4e369ffa2707ae757692508959ac4a2421997
Lucas Peel <81769195+JavascriptCoder999@users.noreply.github.com>
2021-04-17 14:24:22 +0100

    Update proj.cs

--- proj.cs
+++ proj.cs
@@ -1,1 +1,17 @@
-Console.WriteLine("");
+using System;
+namespace ComputerCrash
+{
+  class Program
+  {
+    static void Main(string[]Args)
+    {
+            int i = 0;
+      while (i < 1000000000000000000) 
+      {
+        Console.WriteLine(i);
+        i++;
+      }
+    }
+  }
+}
+//this is a computer crash 😀
