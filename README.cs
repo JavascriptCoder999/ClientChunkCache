b68ccaa8ae75c775d849e82d38586f7ffc5db40d
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-04-25 16:07:36 +0100

    Update README.cs

--- README.cs
+++ README.cs
@@ -1,4 +1,5 @@
 //this is my repository for programming computer crashes
 //look at the license
 //i hopeyou like it
-//you can also look at the wiki noticeboard for questions and bugfixes
+//you can also look at the wiki noticeboard for questions and bugfixes here
+//file:///C:/Users/ianpe/Downloads/wiki.html
