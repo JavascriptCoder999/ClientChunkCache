a32be48e55284e41f7b8e62ffde885e0d9e6bd35
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-04-25 16:03:06 +0100

    Update Mathoverload.cs

--- Mathoverload.cs
+++ Mathoverload.cs
@@ -2,3 +2,6 @@ string name="Overload";
 while(true){
 for(int Overload=0; Overload<1; Overload++)
 }
+//this crashes the computer
+//because the number adds one every tick (about one tenth of a second) to the customized sting variable "Overload"
+//and is too much for the computer to handle
