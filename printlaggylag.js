5095d285b223004826c335e746d0a840e50b5fd6
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-05-07 18:59:08 +0100

    Create printlaggylag.js

---
+++ printlaggylag.js
@@ -0,0 +1,8 @@
+if($this.scene.state()=="PLAY"){
+   window.alert("press space to crash your computer temporarily")
+    if(isKeyPressed(Keys.space)){
+      while(true){
+        print("dummy text");
+      }  
+   }
+}
