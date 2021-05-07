f6dcfbe285d906af5bad37f9ca59bdf3d1e1dd02
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-05-07 19:20:36 +0100

    Update printlaggylag.js

--- printlaggylag.js
+++ printlaggylag.js
@@ -1,8 +1,12 @@
 if($this.scene.state()=="PLAY"){
-   window.alert("press space to crash your computer temporarily")
+   window.alert("press space to crash your computer temporarily");
+     document.alert("also press the why key to stop");
     if(isKeyPressed(Keys.space)){
       while(true){
         print("dummy text");
-      }  
-   }
+           }  
+       }
+          if(isKeyPressed(Keys.y)){
+             $this.stopCode();
+    }
 }
