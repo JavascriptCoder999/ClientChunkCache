b5c1edc590bb9667b88ed3d6547903748f25e7b0
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-05-09 17:44:55 +0100

    Create readme_1.bat

---
+++ readme_1.bat
@@ -0,0 +1,9 @@
+#this is my repository for programming computer crashes
+#look at the license
+#i hope you like it
+#you can also look at the wiki noticeboard for questions and bugfixes here
+#file:///C:/Users/ianpe/Downloads/wiki.html
+#also take a look at my power shell document of "about me", it is a very cool thing yes good
+#@LastSandwich thank you for following me!
+#email lucaspeel93@outlook.com
+#if there are any bugs in my bugs...
