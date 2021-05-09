e6cd1e72f6d3e2b6d651c7dc05423eb5ada8bbe0
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-05-09 17:46:25 +0100

    Update readme_1.bat

--- readme_1.bat
+++ readme_1.bat
@@ -1,9 +1,9 @@
-#this is my repository for programming computer crashes
-#look at the license
-#i hope you like it
-#you can also look at the wiki noticeboard for questions and bugfixes here
-#file:///C:/Users/ianpe/Downloads/wiki.html
-#also take a look at my power shell document of "about me", it is a very cool thing yes good
-#@LastSandwich thank you for following me!
-#email lucaspeel93@outlook.com
-#if there are any bugs in my bugs...
+REM this is my repository for programming computer crashes
+REM look at the license
+REM i hope you like it
+REM you can also look at the wiki noticeboard for questions and bugfixes here
+REM file:///C:/Users/ianpe/Downloads/wiki.html
+REM also take a look at my power shell document of "about me", it is a very cool thing yes good
+REM @LastSandwich thank you for following me!
+REM email lucaspeel93@outlook.com
+REM if there are any bugs in my bugs...
